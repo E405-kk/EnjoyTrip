e5b7502d59b8f8452541e66cf38838a9acedff4f
jeongsol <rnjsjsol0121@gmail.com>
2024-05-20 23:48:13 +0900

    240520 23:47 jeongsol main-detail-css-modified

--- final_05/src/components/common/VRecommend.vue
+++ final_05/src/components/common/VRecommend.vue
@@ -1,12 +1,22 @@
 <script setup>
 import { detailMonthly, detailRank } from "@/api/map";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch } from "vue";
 const props = defineProps({
   idx: Number,
   type: String,
 });
 const recommend = ref({});
-var map;
+let map;
+let marker;
+
+// recommend 객체의 변경 사항을 감지하여 지도 업데이트
+watch(recommend, (newVal) => {
+  if (newVal.latitude && newVal.longitude) {
+    const position = new kakao.maps.LatLng(newVal.latitude, newVal.longitude);
+    map.setCenter(position);
+    marker.setPosition(position);
+  }
+});
 onMounted(() => {
   if (window.kakao && window.kakao.maps) {
     initMap();
@@ -57,11 +67,11 @@ const initMap = () => {
   };
   map = new kakao.maps.Map(container, options);
   // 마커가 표시될 위치입니다
-  const markerPosition = new kakao.maps.LatLng(33.450701, 126.570667);
+  const mapPosition = new kakao.maps.LatLng(33.450701, 126.570667);
 
   // 마커를 생성합니다
-  const marker = new kakao.maps.Marker({
-    position: markerPosition,
+  marker = new kakao.maps.Marker({
+    position: mapPosition,
   });
 
   // 마커가 지도 위에 표시되도록 설정합니다
@@ -70,14 +80,30 @@ const initMap = () => {
 </script>
 
 <template>
-  <div class="w-full lg:w-10/12 text-start bg-white shadow-md rounded-lg p-5">
+  <div
+    class="w-full mx-auto my-10 lg:w-10/12 text-center bg-white shadow-md rounded-lg p-5">
     <div class="flex">
-      <div id="map" style="width: 100%; height: 350px"></div>
-      <img :src="recommend.firstImage" alt="" />
+      <div
+        class="w-5/12 mr-3 rounded-lg"
+        id="map"
+        style="width: 100%; height: 500px"></div>
+
+      <img
+        :src="recommend.firstImage"
+        class="w-7/12 rounded-lg"
+        style="height: 500px" />
     </div>
-    <div>{{ recommend.title }}</div>
-    <div>{{ recommend.addr1 }} {{ recommend.addr2 }}</div>
-    <div>{{ recommend.overview }}</div>
+    <div class="mt-5">
+      <div class="text-center text-3xl font-bold mb-3">
+        {{ recommend.title }}
+      </div>
+      <div class="text-center text-xl font-bold mb-3">
+        {{ recommend.addr1 }} {{ recommend.addr2 }}
+      </div>
+      <div class="text-left text-lg border-t pt-3">
+        {{ recommend.overview }}
+      </div>
+    </div>
   </div>
 </template>
 
