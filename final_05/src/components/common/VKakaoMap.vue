ef63487a84532d112aea1c704440d1ca8b3b11ae
taehun <zxader@naver.com>
2024-05-20 23:49:42 +0900

    240520 23:49 taehun chatbot-add

--- final_05/src/components/common/VKakaoMap.vue
+++ final_05/src/components/common/VKakaoMap.vue
@@ -131,7 +131,7 @@ const loadMarkers = () => {
         `   </a>` +
         `<div>${positions.value[i].addr1}</div>` +
         `   <p class="font-normal text-gray-700 mb-3 dark:text-gray-400 overflow-content">${positions.value[i].overview}</p>` +
-        `   <a href="https://search.naver.com/search.naver?&query=${positions.value[i].title}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">` +
+        `   <a href="https://search.naver.com/search.naver?&query=${positions.value[i].title}" class="text-white bg-green-600 hover:bg-green-500 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center  dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">` +
         `       Read more` +
         `       <svg class="-mr-1 ml-2 h-4 w-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>` +
         `   </a>`;
@@ -140,11 +140,11 @@ const loadMarkers = () => {
         position: markers.value[i].n,
         map: map,
       });
-
+      clusterer.clear();
       var div1 = document.createElement("div");
       div1.setAttribute(
         "class",
-        "custom-overlay bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700"
+        "custom-overlay z-100 bg-white shadow-md border border-gray-200 rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700"
       );
 
       var img = document.createElement("img");
@@ -163,6 +163,7 @@ const loadMarkers = () => {
 
       closeBtn.onclick = function () {
         customOverlay.setMap(null);
+        loadMarkers();
       };
       div1.appendChild(img);
       div1.appendChild(div2);
